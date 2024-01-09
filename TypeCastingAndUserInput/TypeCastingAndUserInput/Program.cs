399090ce80395cc136aebc5fd956271afbfe7266
Nathan Strang <n.strang95@gmail.com>
2024-01-09 13:20:57 +0000

    Adding Solution File

---
+++ TypeCastingAndUserInput/TypeCastingAndUserInput/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TypeCastingAndUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type casting is converting a value to another data type
+
+            double a = 3.14;
+            int b = Convert.ToInt32(a); // converts the double to an int and assigns to new variable
+
+            Console.WriteLine(b);
+            Console.WriteLine(a.GetType()); //The type of variable of a - system.double will be output
+            Console.WriteLine(b.GetType()); //The type of variable of B - system.Int32 will be output
+            Console.ReadKey();
+        }
+    }
+}
